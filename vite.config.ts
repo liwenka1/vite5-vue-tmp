c83555c975d94e688b04f9cfbc506a6a866f4ef4
liwenkai <2020583117@qq.com>
2023-11-23 22:13:39 +0800

    vite 配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,49 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: { '@': path.resolve(__dirname, 'src') }
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+
+  return {
+    plugins: [vue()],
+    base: env.VITE_PUBLIC_PATH,
+    resolve: {
+      alias: { '@': path.resolve(__dirname, 'src') }
+    },
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_APP_BASE_URL,
+          changeOrigin: true,
+          secure: true,
+          rewrite: (path) => path.replace('/^/api/', '/api')
+        }
+      }
+    },
+    build: {
+      chunkSizeWarningLimit: 2000, // 消除打包大小超过500kb警告
+      outDir: 'dist',
+      minify: 'terser',
+      terserOptions: {
+        compress: {
+          keep_infinity: true, // 防止 Infinity 被压缩成 1/0，这可能会导致 Chrome 上的性能问题
+          drop_console: true, // 去除console
+          drop_debugger: true // 去除debugger
+        },
+        format: {
+          comments: false // 去除注释
+        }
+      },
+      // 静态资源打包到dist下的不同目录
+      rollupOptions: {
+        output: {
+          chunkFileNames: 'static/js/[name]-[hash].js',
+          entryFileNames: 'static/js/[name]-[hash].js',
+          assetFileNames: 'static/[ext]/[name]-[hash].[ext]'
+        }
+      }
+    }
   }
 })
