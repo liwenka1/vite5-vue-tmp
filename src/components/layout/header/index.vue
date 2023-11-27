fc6556ca81ec62529ff09f9fd8cfdc34f96cbade
liwenkai <2020583117@qq.com>
2023-11-27 15:02:39 +0800

    修改文件名大小写

---
+++ src/components/layout/header/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <Theme />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import Theme from '@/components/theme/index.vue'
+</script>
+
+<style lang="scss" scoped></style>
